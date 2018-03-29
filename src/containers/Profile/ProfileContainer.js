ffe30cd5e21c1436224b37bc9161c2c94b2c2b83
cubamex209 <omarh209@gmail.com>
2018-03-29 11:30:20 -0700

    login page

---
+++ src/containers/Profile/ProfileContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './ProfileContainers.css';
+
+class ProfileContainers extends Component {
+  render() {
+    return (
+
+      <div> This is My Profile Container </div>
+
+    )
+    }
+  };
+
+
+export default ProfileContainers;
